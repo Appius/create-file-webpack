e5257a98697555985ebd1f11ad310c994e5a9c9a
Appius <appius@outlook.com>
2017-10-09 17:33:01 +0300

    Fixed index.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 'use strict';
-var DtsBundlePlugin = (function () {
-    const dts = require('dts-bundle');
+var CreateFilePlugin = (function () {
+    const write = require('write');
+    const path = require('path');
 
-    function DtsBundlePlugin(options){
+    function CreateFilePlugin(options){
         if (options === void 0) { options = {}; }
         this.options = options;
     }
 
-    DtsBundlePlugin.prototype.apply = function (compiler) {
+    CreateFilePlugin.prototype.apply = function (compiler) {
         const _this = this;
 
         compiler.plugin('done', function() {
-            dts.bundle(_this.options);
+            const fullPath = path.join(_this.options.path, _this.options.fileName);
+            write.sync(fullPath, _this.options.content);
         });
     };
 
-    return DtsBundlePlugin;
+    return CreateFilePlugin;
 })();
 
-module.exports = DtsBundlePlugin;
+module.exports = CreateFilePlugin;
