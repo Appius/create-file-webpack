17c8c8d4b3aedbbd6c64bf42f006bf1527e2faff
Vladyslav Ushakov <appius@outlook.com>
2018-12-02 13:08:30 +0200

    added usage for new webpack api, remove garbage from package

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 'use strict';
 var CreateFilePlugin = (function () {
-    const write = require('write');
-    const path = require('path');
+  const write = require('write');
+  const path = require('path');
 
-    function CreateFilePlugin(options){
-        if (options === void 0) { options = {}; }
-        this.options = options;
+  function CreateFilePlugin(options){
+    if (options === void 0) {
+      throw new Error(`Please provide 'options' for the CreateFilePlugin config`);
     }
 
-    CreateFilePlugin.prototype.apply = function (compiler) {
-        const _this = this;
+    if (options.path == null) {
+      throw new Error(`Please provide 'options.path' in the CreateFilePlugin config`);
+    }
 
-        compiler.plugin('done', function() {
-            const fullPath = path.join(_this.options.path, _this.options.fileName);
-            write.sync(fullPath, _this.options.content);
-        });
-    };
+    if (options.fileName == null) {
+      throw new Error(`Please provide 'options.fileName' in the CreateFilePlugin config`);
+    }
 
-    return CreateFilePlugin;
+    if (options.content == null) {
+      throw new Error(`Please provide 'options.content' in the CreateFilePlugin config`);
+    }
+
+    this.options = options;
+  }
+
+  function _createFile(filePath, fileName, content) {
+    return () => {
+      const fullPath = path.join(filePath, fileName);
+      write.sync(fullPath, content);
+    }
+  }
+
+  CreateFilePlugin.prototype.apply = function (compiler) {
+    const createFile = () => _createFile(this.options.path, this.options.fileName, this.options.content);
+
+    if (!!compiler.hooks) {
+      compiler.hooks.done.tap('CreateFileWebpack', createFile());
+    } else {
+      compiler.plugin('done', createFile());
+    }
+  };
+
+  return CreateFilePlugin;
 })();
 
 module.exports = CreateFilePlugin;
